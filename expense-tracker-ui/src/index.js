4b7617332bb6c3ca375a3fbf7bd45fbdb76a7f21
itzish1999 <ismaeel.butt19@gmail.com>
2022-04-20 15:36:53 -0400

    feat(React/Express app connected) Connected Database to React front end via axios.

--- expense-tracker-ui/src/index.js
+++ expense-tracker-ui/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorkerRegistration';
 ReactDOM.render(
   <BrowserRouter>
   <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
+serviceWorker.unregister();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
